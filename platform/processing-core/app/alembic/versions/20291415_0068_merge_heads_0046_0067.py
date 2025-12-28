eb1809b283137b54feabfdaa7b65dcf421dbd8f1
Tor08000 <Tor_08@mail.ru>
2025-12-28 12:53:43 +0300

    Add merge revision for alembic heads

---
+++ platform/processing-core/app/alembic/versions/20291415_0068_merge_heads_0046_0067.py
@@ -0,0 +1,21 @@
+"""merge heads 0046 and 0067.
+
+Revision ID: 20291415_0068_merge_heads_0046_0067
+Revises: 20290510_0046_accounting_export_erp_ack, 20291410_0067_subscription_billing_job_type
+Create Date: 2029-04-15 00:00:00.000000
+"""
+
+from __future__ import annotations
+
+revision = "20291415_0068_merge_heads_0046_0067"
+down_revision = ("20290510_0046_accounting_export_erp_ack", "20291410_0067_subscription_billing_job_type")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
