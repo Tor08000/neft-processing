eb5351cafe6b95a6adfe7e5774266458343a6d7c
Tor08000 <Tor_08@mail.ru>
2026-03-01 18:28:13 +0300

    Merge Alembic heads after 20300130_0206

---
+++ platform/processing-core/app/alembic/versions/28d39dce919f_merge_heads_after_internal_ledger_v1_.py
@@ -0,0 +1,25 @@
+"""merge heads after internal_ledger_v1 and 7cd06db99ada
+
+Revision ID: 28d39dce919f
+Revises: 20300130_0206_internal_ledger_v1_backbone, 7cd06db99ada
+Create Date: 2026-03-01 15:15:03.735623
+"""
+
+from __future__ import annotations
+
+
+revision = "28d39dce919f"
+down_revision = (
+    "20300130_0206_internal_ledger_v1_backbone",
+    "7cd06db99ada",
+)
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
